b1a1db5462a19fbb9f5bd22ebaced070d97a5580
ANONYMOUSx46 <liamjudedewet85@gmail.com>
2025-01-29 00:04:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,12 @@ export default defineConfig({
   build: {
     outDir: 'build', // Ensure this matches your Netlify publish directory
     sourcemap: true, // Enable source maps for easier debugging
+    assetsDir: 'assets', // Ensure assets are placed in the assets directory
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-    }
+    },
   },
   server: {
     port: 3000, // Optional: specify the development server port
