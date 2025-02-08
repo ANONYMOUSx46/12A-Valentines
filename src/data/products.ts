120f400ba4f95d139c563ebc5be0d636b13354fb
ANONYMOUSx46 <liamjudedewet85@gmail.com>
2025-02-08 17:32:23 +0200

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -22,63 +22,70 @@ export const products = [
     image:  oatcrunchieImage
   },
   {
-    id: 2,
-    name: " Smash Burger Special with chips",
+    id: 3,
+    name: "Vienna Parcels",
     price: 40.00,
-    description: "Delicious smash burger filled with exotic flavours featuring hot chips. Can purchase a single burger for R30 and the hot chips for R15",
+    description: "Delicious Vienna parcels filled with exotic flavours",
     image: girlThemeImage 
 	},
   {
-    id: 3,
+    id: 4,
+    name: "Chip Rolls",
+    price: 40.00,
+    description: "Rolls filled with hot chips perfect for a quick meal",
+    image: girlThemeImage 
+	},
+  {
+    id: 5,
     name: "Ribbon Rose Bouquet",
     price: 20.00,
     description: "Beautiful handcrafted ribbon rose bouquet 8 flowers in a bouquet, R5 for a single flower",
     image: ribbonRoseImage
   },
     {	
-    id: 4,
-    name: "Milkshake 500ml",
+    id: 6,
+    name: "Ice Coffee 500ml",
     price: 15.00,
-    description: "Sweet and tasty Milkshake in a 500ml cup, in various flavours",
+    description: "Refreshing Iced Coffee to sip on throughout your day",
     image: popCornImage
     },
   {	
-    id: 5,
+    id: 7,
     name: "Buscuits",
     price: 15.00,
     description: "A delicious butter buscuit to go with your iced coffee",
     image: "https://cdn.qwenlm.ai/output/e6c32d1f-21bc-4a2f-b447-09eb92b016c8/t2i/7860f881-7678-4ced-b617-5757e0972611/c4ecad6d-192c-4dfc-92d8-8dfe136ae2bb.png"
   },
   {	
-    id: 6,
+    id: 8,
     name: "Muffins",
     price: 5.00,
     description: "Freshly baked muffins ready to be eaten",
     image: "https://cdn.qwenlm.ai/output/e6c32d1f-21bc-4a2f-b447-09eb92b016c8/t2i/60f57b22-d027-4e79-a13c-839b716ec067/daa2a3b8-3518-47d3-aedc-91ba4c77f62c.png"
   },
    {	
-    id: 7,
+    id: 9,
     name: "Fresh Popcorn",
     price: 5.00,
     description: "Freshly popped popcorn",
     image: milkshakeImage
   },
   {
-    id: 8,
+    id: 10,
     name: " Girl-Themed Raffle Bundle",
     price: 10.00,
     description: "Win amazing prizes specifically girl-themed R10 per ticket",
     image:  smashburgerImage
   },
 	  {
-    id: 10,
+    id: 11,
     name: " Boy-Themed Raffle Bundle",
     price: 10.00,
     description: "Win exciting prizes specifically boy-themed R10 per ticket",
     image: boyThemeImage
   },
    {
-    id: 11,
+    id: 12,
     name: "Car Wash",
     price: 50.00,
     description: "Car wash for the hard working teachers that would like their car to shine!",
